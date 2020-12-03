df8e158942bc12f95e6e3bf98bb019ecb344f1b3
Andrey Misyura <magway2k@mail.ru>
2020-12-03 19:57:00 +0300

    first working version

--- run.rb
+++ run.rb
@@ -5,43 +5,33 @@ require 'net/dav'
 require 'tempfile'
 
 class File
-  def each_chunk(chunk_size=2**20)
-    yield self.read(chunk_size) until self.eof?
+  def each_chunk(chunk_size = 2**20)
+    yield read(chunk_size) until eof?
   end
 end
 
-url = 'http://127.0.0.1:8080/remote.php/dav/files/admin/'
+url = 'http://127.0.0.1:8080/remote.php/dav/uploads/admin/'
 user = 'admin'
 pass = 'admin'
 folder = (0..16).to_a.map { |_a| rand(16).to_s(16) }.join
-local_file = './001.mp4'
-file_name = 'part'
+file_name = 'file.mp4'
 dav = Net::DAV.new(url, curl: false)
 dav.verify_server = false
 dav.credentials(user, pass)
 dav.mkdir(folder)
-# dav.find('.', recursive: true, suppress_errors: true, filename: /\.*/) do |item|
-#   puts 'Checking: ' + item.url.to_s
-# end
-# chunk_size = 2**20
-# number_chank = 1
-# File.open(local_file).each(nil, chunk_size) do |chunk|
-#   dav.put("./#{folder}/#{number_chank}#{file_name}", chunk, chunk.size)
-#   sleep 1
-#   number_chank += 1
-# end
-
-
-File.open(local_file, 'rb') do |stream|
+File.open("./#{file_name}", 'rb') do |stream|
   number_chank = 1
   stream.each_chunk do |chunk|
-    tmp = Tempfile.new("#{number_chank}")
+    tmp = Tempfile.new(number_chank.to_s)
     tmp.puts chunk
-    dav.put("./#{folder}/#{number_chank}#{file_name}", tmp.path, chunk.size)
+    dav.put("./#{folder}/#{number_chank}", tmp.path, chunk.size)
     tmp.close
     number_chank += 1
   end
 end
+dav.move("#{folder}/.file", "http://127.0.0.1:8080/remote.php/dav/files/admin/#{file_name}")
 # dav.put("./#{folder}/#{file_name}", stream, File.size(local_file))
 # sleep 15
 # dav.delete(folder)
+# curl -X MOVE -u admin:admin --header 'Destination:http://127.0.0.1:8080/remote.php/dav/files/admin/file.zip' http://127.0.0.1:8080/remote.php/dav/uploads/admin/681bb5c5980ca7a66/.file
+
